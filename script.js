f66505ca0fab11dc483aeedcddff1217ffb782ac
plantsX <84574289+DaviAUJ@users.noreply.github.com>
2023-10-02 16:01:26 -0300

    nn sei q nome dar, fiz um geral aí

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // Gera um número aleatório entre min e max, btw essa func não é pura
 const randint = (min) => (max) => Math.floor(Math.random() * (max - min + 1) + min)
 
+// Autoexplicativo
 const divisaoInteira = (dividendo) => (divisor) => Math.floor(dividendo / divisor)
 
 // Cria elemento com já uma ID, este foi o melhor jeito de fazer que achei de fazer os dois ao mesmo tempo sem furar o paradigma
-const criarElemComID = (tipo) => (id) => Object.assign(document.createElement("div"), {id: id})
+const criarElemComID = (tipo) => (id) => Object.assign(document.createElement(tipo), {id: id})
 
+// Cria a matriz do campo, que é a representação dele no código usando 1 para bombas e 0 para espaços vazios
+// Não consegui fazer de um jeito funcional usando sistema de seed
 const criarMatrizDoCampo = (dimensao) => (percentual) => (cont=1) => {
     const gerarLinha = (tamanho) => (percentual) => (cont=1) => {
         if(cont == tamanho) {
@@ -28,10 +31,14 @@ const criarMatrizDoCampo = (dimensao) => (percentual) => (cont=1) => {
     return [ gerarLinha(dimensao)(percentual)(), ...criarMatrizDoCampo(dimensao)(percentual)(cont + 1)]
 }
 
-const mostrarNumero = (elemento) => (matriz) => () => {
+// Essa função basicamente se resume ao clique, ele verifica quantas bombas tem ao redor, dps 
+// espalha ela mesmo caso o quadrado clicado seja 0, fazendo um efeito dominó até encontrar uma casa com pelo menos uma bomba ao redor
+const mostrarNumero = (elemento) => (matriz) => (cont=0) => () => {
+    // Esta função é usada para "olhar" quantas bombas existem ao redor de um certo quadrado
     const verificar = (id) => (matriz) => (contagem=0) => {
-        const idarray = id.split(" ") // botar o map aqui por algum motivo nn funciona
-        const idint = [parseInt(idarray[0]), parseInt(idarray[1])]
+        const idint = [ parseInt(id[0]), parseInt(id[2]) ]
+
+        // Ordem de checagem
         const ordem = [
             [0,1],
             [1,1],
@@ -45,25 +52,78 @@ const mostrarNumero = (elemento) => (matriz) => () => {
 
         if(contagem==7)   {
             try{
-                const tembomba = matriz[idint[0]+ordem[contagem][0]][idint[1]+ordem[contagem][1]]
+                const tembomba = matriz[idint[1]+ordem[contagem][1]][idint[0]+ordem[contagem][0]]
+                
+                if(typeof tembomba == "undefined") { return 0 } // tive que fazer essa gambiarra aqui desculpa
+
                 return tembomba
 
             }catch(err){
                 return 0
+
             }
         }
     
         try{
-            const tembomba = matriz[idint[0]+ordem[contagem][0]][idint[1]+ordem[contagem][1]]
+            const tembomba = matriz[idint[1]+ordem[contagem][1]][idint[0]+ordem[contagem][0]]
             
+            if(typeof tembomba == "undefined") { return verificar(id)(matriz)(contagem+1) } // aqui tbm
+
             return verificar(id)(matriz)(contagem+1) + tembomba
             
         }catch(err){
             return verificar(id)(matriz)(contagem+1)
+
         }
     }
 
-elemento.innerHTML = verificar(elemento.id)(matriz)()
+    const bombasAdj = verificar(elemento.id)(matriz)()
+
+    elemento.innerHTML = bombasAdj
+    elemento.setAttribute("class", "verificado") // classe necessaria, se ela não existe nn teria como saber 
+                                                 // se o quadrado já foi verificado criando um loop infinito
+
+    // Caso seja a casa tenha zero bombas ao redor a função vai se replicar nos quadrados proximos até encontrar um quadrado com 
+    // mais de uma bomba proxima
+    if(bombasAdj == 0) {
+        elemento.style.backgroundColor = "#FFFFFF"
+
+        const idint = [ parseInt(elemento.id[0]), parseInt(elemento.id[2]) ]
+
+        // Ordem de checagem
+        const ordem = [
+            [0,1],
+            [1,0],
+            [0,-1],
+            [-1,0],
+        ]
+    
+        if(cont == 3) {
+            try {
+                // Pega o quadrado adjacente da vez
+                const adjacente = document.getElementById(`${idint[0] + ordem[cont][0]} ${idint[1] + ordem[cont][1]}`)
+
+                if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0)() }
+
+                return
+
+            } catch(err) { return }
+            
+        }
+
+        try{
+            // Pega o quadrado adjacente da vez
+            const adjacente = document.getElementById(`${idint[0] + ordem[cont][0]} ${idint[1] + ordem[cont][1]}`)
+
+            if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0)() }
+
+            mostrarNumero(elemento)(matriz)(cont + 1)()
+
+        } catch(err) {
+            mostrarNumero(elemento)(matriz)(cont + 1)()
+
+        }
+    }
 }
 
 // Essa função é usada para carregar os N quadrados para formar a grade do campo minado
@@ -72,25 +132,14 @@ const carregar = (elementoHTML) => (dimensao) => (cont=0) => {
     if(cont == dimensao ** 2) { return }
 
     //Define o elemento com ID baseado na coordenada
-    const novoElem = criarElemComID("div")(`${divisaoInteira(cont)(dimensao)} ${(cont) % dimensao}`)
+    const novoElem = criarElemComID("div")(`${(cont) % dimensao} ${divisaoInteira(cont)(dimensao)}`)
 
     // Insere o novo elemento dentro do elemento passado como parametro
-    novoElem.addEventListener("click",mostrarNumero(novoElem)(teste))
+    novoElem.addEventListener("click", mostrarNumero(novoElem)(teste)())
     elementoHTML.appendChild(novoElem)
 
     carregar(elementoHTML)(dimensao)(cont + 1)
 }
-
-
-// TODO = a fazer
-// TODO: uma forma de fazer essa função ser pura pq nn consegui fazer usando sistema de seed, ent o jeito é usar randint()
-// TODO: também tentar fazer uma forma de fazer com o tanto de bombas sejam uniformes pq do jeito q tá fica variando, 10 - 20%, 20 - 25%, 30 - 30%
-//       se o cara der azar pode até pegar um campo inteiro
-// TODO: Fazer uma forma de o quadrado que o jogador clicar seja livre de bombas e que seus quadrados vizinhos tbm não tenham
-// TODO: tentar fazer uma forma de que bombas nunca façam um anel, onde a quadrado do meio fique isolado
-//       Esse nn tem tanta urgencia pq no original isso podia acontecer
-
-// Função usada para gerar uma matriz que representa o campo no código, 1 para bombas, 0 para espaços vazia, vou dormir agora boa noite
 
 // Reutilização de carregar 
 const carregarCampo = carregar(document.getElementById("campo"))
@@ -99,16 +148,26 @@ const carregarCampo = carregar(document.getElementById("campo"))
 // Para mudar o arquivo CSS não adianta tem que furar o paradigma
 const escolherdificuldade = (id) => {
     const style =  document.documentElement.style
-    if (id == "facil")  {carregarCampo(10)() 
-        style.setProperty("--dimensao", 10)}
-    else if (id == "intermediario") {carregarCampo(20)() 
-        style.setProperty("--dimensao", 20)}
-    else if (id == "dificil") {carregarCampo(30)() 
-        style.setProperty("--dimensao", 30)}
+
+    if (id == "facil")  {
+        carregarCampo(10)() 
+        style.setProperty("--dimensao", 10)
+
+    } else if (id == "intermediario") {
+        carregarCampo(20)() 
+        style.setProperty("--dimensao", 20)
+
+    } else if (id == "dificil") {
+        carregarCampo(30)() 
+        style.setProperty("--dimensao", 30)
+
+    }
+
     document.getElementById("menu").style.visibility ="hidden"
     document.getElementById("campo").style.visibility="visible"
 }
 
-const teste = criarMatrizDoCampo(10)(20)()
+const teste = criarMatrizDoCampo(10)(12)()
 
 console.log(teste)
+
