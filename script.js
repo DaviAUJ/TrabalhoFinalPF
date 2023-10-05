cf3c3658c3137c6c021de255fc886394a3813dc1
vitordias32 <jvds3246@gmail.com>
2023-10-05 11:49:28 -0300

    Lógica da área de isenção de bombas

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ const divisaoInteira = (dividendo) => (divisor) => Math.floor(dividendo / diviso
 // Cria elemento com já uma ID, este foi o melhor jeito de fazer que achei de fazer os dois ao mesmo tempo sem furar o paradigma
 const criarElemComID = (tipo) => (id) => Object.assign(document.createElement(tipo), {id: id})
 
+// retorna um true se todos os filhos de um elemento estão sem classe, caso pelo menos um tenha ele retorna false
+const quantosFilhosComAClasse = (elem) => (nomeClasse) => {
+    const listaFilhos = [...elem.children] // precisa fazer uma copia aqui senao nao funciona
+    const temp = listaFilhos.filter(
+        (x) => x.className == nomeClasse
+    )
+
+    return temp.length
+}
+
+// funcao que adiciona event listener em todos os filhos de um elemento
+const AELemTodosFilhos = (elem) => (tipo) => (funcao) => {
+    const aux = ([x, ...xs]) => {
+        if(xs.length == 0){x.addEventListener(tipo, funcao)}
+        else {
+            x.addEventListener(tipo, funcao)
+            
+            aux(xs)
+        }
+    }
+
+    aux(elem.childNodes)
+}
+
+// funcao que tira event listener de todos os filhos de um elemento
+const RELemTodosFilhos = (elem) => (tipo) => (funcao) => {
+    const aux = ([x, ...xs]) => {
+        if(xs.length == 0){x.removeEventListener(tipo, funcao)}
+        else {
+            x.removeEventListener(tipo, funcao)
+            
+            aux(xs)
+        }
+    }
+
+    aux(elem.childNodes)
+}
 
 const editarmatriz = (matriz)=>(coodx)=>(coody)=>(novoelem)=>{
     return matriz.map(
@@ -25,7 +62,6 @@ const editarmatriz = (matriz)=>(coodx)=>(coody)=>(novoelem)=>{
 }
 
 // Cria a matriz do campo, que é a representação dele no código usando 1 para bombas e 0 para espaços vazios
-// Não consegui fazer de um jeito funcional usando sistema de seed
 const criarMatrizDoCampo = (dimensao) => (cont=1) => {
     const gerarLinha = (tamanho) => (cont=1) => {
         if(cont == tamanho) {  
@@ -40,23 +76,25 @@ const criarMatrizDoCampo = (dimensao) => (cont=1) => {
     return [ gerarLinha(dimensao)(), ...criarMatrizDoCampo(dimensao)(cont + 1)]
 }
 
-
+// Não consegui fazer de um jeito funcional usando sistema de seed
+// funcao que server pra posicionar as bombas em locais aleatorios
 const colocarbombas = (matriz) =>(nbombas)=>(contagem=1)=>{
     const x = randint(0)(matriz.length-1)
     const y = randint(0)(matriz.length-1)
-    if(nbombas==contagem) {
-        if(matriz[y][x]==0){return editarmatriz(matriz)(x)(y)(1)}
 
-        else if (matriz[y][x]==1){return colocarbombas(matriz)(nbombas)(contagem)}
+    if(nbombas==contagem) {
+        if(matriz[y][x]==0 && typeof matriz[y][x] != 'string'){return editarmatriz(matriz)(x)(y)(1)}
+
+        else {return colocarbombas(matriz)(nbombas)(contagem)}
     }
-    if(matriz[y][x]==0){return colocarbombas(editarmatriz(matriz)(x)(y)(1))(nbombas)(contagem+1)}
-    else if (matriz[y][x]==1){return colocarbombas(matriz)(nbombas)(contagem)}
+    if(matriz[y][x]==0 && typeof matriz[y][x] != 'string'){return colocarbombas(editarmatriz(matriz)(x)(y)(1))(nbombas)(contagem+1)}
+    else {return colocarbombas(matriz)(nbombas)(contagem)}
 
 }
 
 // Essa função basicamente se resume ao clique, ele verifica quantas bombas tem ao redor, dps 
 // espalha ela mesmo caso o quadrado clicado seja 0, fazendo um efeito dominó até encontrar uma casa com pelo menos uma bomba ao redor
-const mostrarNumero = (elemento) => (matriz) => (cont=0) => () => {
+const mostrarNumero = (elemento) => (matriz) => (cont=0) => {
     // Esta função é usada para "olhar" quantas bombas existem ao redor de um certo quadrado
     const verificar = (id) => (matriz) => (contagem=0) => {
         const idint = [ parseInt(id[0]), parseInt(id[2]) ]
@@ -126,7 +164,7 @@ const mostrarNumero = (elemento) => (matriz) => (cont=0) => () => {
                 // Pega o quadrado adjacente da vez
                 const adjacente = document.getElementById(`${idint[0] + ordem[cont][0]} ${idint[1] + ordem[cont][1]}`)
 
-                if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0)() }
+                if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0) }
 
                 return
 
@@ -138,15 +176,41 @@ const mostrarNumero = (elemento) => (matriz) => (cont=0) => () => {
             // Pega o quadrado adjacente da vez
             const adjacente = document.getElementById(`${idint[0] + ordem[cont][0]} ${idint[1] + ordem[cont][1]}`)
 
-            if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0)() }
+            if(adjacente.className != "verificado") { mostrarNumero(adjacente)(matriz)(0) }
 
-            mostrarNumero(elemento)(matriz)(cont + 1)()
+            mostrarNumero(elemento)(matriz)(cont + 1)
 
         } catch(err) {
-            mostrarNumero(elemento)(matriz)(cont + 1)()
+            mostrarNumero(elemento)(matriz)(cont + 1)
 
         }
     }
+}
+
+// funcao geral para quando o jogador clicar em quadrado qualquer
+// ela e usada para identificar o primeiro quadrado clicado e depois criar o campo
+const clicar = (elem) => () => {
+    const campo = document.getElementById("campo")
+    const coordx = parseInt(elem.id[0])
+    const coordy = parseInt(elem.id[2])
+
+    // faz uma verificacao para saber se esse clique foi o primeiro
+    // a area de isencao é identificado como um 2
+    if(quantosFilhosComAClasse(campo)('verificado') == 0) {
+        // aqui nao conseguimos fazer de uma forma funcional tivemos que usar variavel
+        // a area de isencao é identificada por um 0 numa string
+        matrizPrincipal = editarmatriz(matrizPrincipal)(coordx)(coordy)('0')
+        
+        // depois do primeiro clique ai sim coloca as bombas de acordo com a dificuldade
+        if(matrizPrincipal.length == 10) { matrizPrincipal = colocarbombas(matrizPrincipal)((matrizPrincipal.length ** 2) * 0.15)() }
+        else if(matrizPrincipal.length == 20) { matrizPrincipal = colocarbombas(matrizPrincipal)((matrizPrincipal.length ** 2) * 0.20)() }
+        else if(matrizPrincipal.length == 30) { matrizPrincipal = colocarbombas(matrizPrincipal)((matrizPrincipal.length ** 2) * 0.25)() }
+
+        matrizPrincipal = editarmatriz(matrizPrincipal)(coordx)(coordy)(0) // dps volta com o zero numero para nao quebrar os calculos das outras funcoes
+    }
+    
+    console.log(matrizPrincipal)
+    mostrarNumero(elem)(matrizPrincipal)()
 }
 
 // Essa função é usada para carregar os N quadrados para formar a grade do campo minado
@@ -158,7 +222,7 @@ const carregar = (elementoHTML) => (dimensao) => (cont=0) => {
     const novoElem = criarElemComID("div")(`${(cont) % dimensao} ${divisaoInteira(cont)(dimensao)}`)
 
     // Insere o novo elemento dentro do elemento passado como parametro
-    novoElem.addEventListener("click", mostrarNumero(novoElem)(teste)())
+    novoElem.addEventListener("click", clicar(novoElem))
     elementoHTML.appendChild(novoElem)
 
     carregar(elementoHTML)(dimensao)(cont + 1)
@@ -190,5 +254,6 @@ const escolherdificuldade = (id) => {
     document.getElementById("campo").style.visibility="visible"
 }
 
-const teste = colocarbombas(criarMatrizDoCampo(10)())(10)()
-console.log(teste)
+// a gente nao acho uma forma de criar uma area de isencao sem quebrar o paradigma
+// e para isso e preciso declarar essa variavel
+let matrizPrincipal = criarMatrizDoCampo(10)()
